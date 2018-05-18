cd8b12dacd4ea2ff9ea2cc73098d3e5dee1218d2
Yoon Jo Han <yuniegu3@gmail.com>
2018-05-18 15:03:43 -0400

    added the relations to models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+	has_many :tasks
+	has_one :user
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+	has_one :project
+	has_many :tasks
+	has_many :tasks_tags
+	has_many :tags, through: :tasks_tags
 end
