760f4ea98056fa74af9079464a03866756196686
maiya-22 <maiya_o@yahoo.com>
2018-05-19 17:42:50 -0400

    basic frame html in place on views/pages/index.html.erb and media queries in place and linked.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+  layout "pages_layout"
   def index
+    @project = {name: "learn to program"}
+    # @project[:name] = "learn to program"  #note: why did the :name not pass to template when made this way?
   end
 
   def project
   end
 end
+ 
