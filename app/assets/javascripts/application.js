fbac4124a46195043f553f7d16a22c4be4d94311
maiya-22 <maiya_o@yahoo.com>
2018-05-19 23:47:07 -0400

    toggling working for both views. need to fix the stickiFrame's height when toggled.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// require bootstrap
+//= require bootstrap
 //= require rails-ujs
 //= require turbolinks
 //= require view-selectors
-// require_tree .
+//= require_tree .
--- app/assets/javascripts/view-selectors.js
+++ app/assets/javascripts/view-selectors.js
@@ -1,14 +1,30 @@
 console.log('view-selectors.js linked');
 
 // event listeners for the view selector buttons, to change the view option
-const viewControlFrame = document.getElementByid('viewControlFrame');
+const viewControlFrame = document.getElementById('viewControlFrame');
 if (viewControlFrame) {
+  console.log(true);
   viewControlFrame.addEventListener('click', (e) => {
-    let viewOption;
     const { target } = e;
-    if (target.classList.contains('viewSelector')) {
-      viewOption = target.id;
-      console.log('viewOption', viewOption);
+    if (!target.classList.contains('viewSelector')) return null;
+    const viewOption = target.id;
+    console.log(viewOption);
+    const pageFrame = document.getElementById('pageFrame');
+    const stickiFrame = document.getElementById('stickiFrame');
+    const listFrame = document.getElementById('listFrame');
+    if (viewOption === 'rowToggle') {
+      pageFrame.classList.add('columnFrame');
+      pageFrame.classList.remove('rowFrame');
+    } else if (viewOption === 'columnToggle') {
+      pageFrame.classList.add('rowFrame');
+      pageFrame.classList.remove('columnFrame');
+      stickiFrame.classList.add('rowItem');
+      listFrame.classList.add('rowItem');
     }
+    // and always do this:
+    stickiFrame.classList.toggle('first');
+    stickiFrame.classList.toggle('second');
+    listFrame.classList.toggle('first');
+    listFrame.classList.toggle('second');
   });
 }
