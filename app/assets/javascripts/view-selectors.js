c83f9d72a6c8ced9bb977719590ccc844e003a7b
maiya-22 <maiya_o@yahoo.com>
2018-05-20 11:58:10 -0400

    saving progress before commenting code

--- app/assets/javascripts/view-selectors.js
+++ app/assets/javascripts/view-selectors.js
@@ -3,15 +3,14 @@ console.log('view-selectors.js linked');
 // event listeners for the view selector buttons, to change the view option
 const viewControlFrame = document.getElementById('viewControlFrame');
 if (viewControlFrame) {
-  console.log(true);
   viewControlFrame.addEventListener('click', (e) => {
     const { target } = e;
+    // if the event target is not one of the viewSelector buttons, break out of the function:
     if (!target.classList.contains('viewSelector')) return null;
     const viewOption = target.id;
+    // pageFrame is the div surrounding the stickiFrame and listFrame
     const container = document.getElementById('pageFrame');
-    const items = document.getElementsByClassName('pageFrame');
     if (viewOption === 'columnToggle') {
-      console.log('columnToggle');
       if (container.classList.contains('columnFrame')) {
         container.classList.toggle('columnFrameReverse');
       } else {
