3a0353a914e9d3e66c80af394b59db89c8a24bc8
maiya-22 <maiya_o@yahoo.com>
2018-05-20 11:58:10 -0400

    saving progress before commenting code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   layout "pages_layout"
   def index
-    @project = {name: "learn to program"}
+    @project = {name: ""}
     # @project[:name] = "learn to program"  #note: why did the :name not pass to template when made this way?
   end
 
