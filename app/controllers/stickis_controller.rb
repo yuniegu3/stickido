3d7b7afab34de66162a036bbe3c268a9de4419e7
Yoon Jo Han <yuniegu3@gmail.com>
2018-05-23 16:19:57 -0400

    stickis are in order now, recently created stickis will be on top

--- app/controllers/stickis_controller.rb
+++ app/controllers/stickis_controller.rb
@@ -3,7 +3,7 @@ class StickisController < ApplicationController
 
 #     stickis GET    /stickis(.:format) stickis#index
 	def index
-		@stickis = Sticki.where(project_id: params[:project_id]).order(id: :asc)
+		@stickis = Sticki.where(project_id: params[:project_id]).order(created_at: :desc)
 	end
 
 #      sticki GET    /stickis/:id(.:format) stickis#show
